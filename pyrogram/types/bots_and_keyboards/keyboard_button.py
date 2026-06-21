72ef7a175e0754f96c9047c7582da43707c126a9
lorddzik <azerlaeron@gmail.com>
2026-06-21 08:43:09 +0700

    feat: add KeyboardButton class for reply keyboard button support

--- pyrogram/types/bots_and_keyboards/keyboard_button.py
+++ pyrogram/types/bots_and_keyboards/keyboard_button.py
@@ -16,6 +16,8 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+from typing import Union, Optional
+
 from pyrogram import raw, types
 from ..object import Object
 
